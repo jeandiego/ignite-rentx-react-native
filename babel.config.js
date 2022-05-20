d9fd72614ac4526106de95981bd72e452995a528
Jean Diego <jeandiego.ds@gmail.com>
2022-05-19 21:03:15 -0300

    feat: added root import to project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    plugins: [
+      [
+        "babel-plugin-root-import",
+        {
+          paths: [
+            {
+              rootPathSuffix: "src",
+              rootPathPrefix: "~/"
+            },
+          ]
+        }
+      ]
+    ],
     presets: ['babel-preset-expo']
   };
 };
