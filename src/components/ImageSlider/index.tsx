f6ca2fcdb5f30cb669d6d297b51e589f7ff5b99c
Jean Diego <jean.santo@db1.com.br>
2022-07-04 08:21:42 -0300

    feat: create image slider component

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
     Container,ImageIndexes, ImageIndex, CarImageWrapper,CarImage
@@ -8,22 +9,50 @@ interface Props {
     imagesUrl: string[];
 }
 
-export function ImageSlider({ imagesUrl }: Props) {
-  return (
-      <Container>
-        <ImageIndexes>
-            <ImageIndex active={true} />
-            <ImageIndex active={false} />
-            <ImageIndex active={false} />
-            <ImageIndex active={false} />
-        </ImageIndexes>
+interface IChangeImageProps {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+}
 
+export function ImageSlider({ imagesUrl }: Props) {
+    const [imageIndex, setImageIndex] = useState(0);
+
+const onIndexChanged = useRef((info: IChangeImageProps) => {
+    const index = info.viewableItems[0].index;
+    setImageIndex(index);
+})
+
+const renderItem = useCallback(({item}) => {
+    return  (
         <CarImageWrapper>
             <CarImage 
-                source={{uri: imagesUrl[0] }}
+                source={{uri: item }}
                 resizeMode='contain'
             />
         </CarImageWrapper>
+    )
+}, [])
+
+  return (
+      <Container>
+        <ImageIndexes>
+            {
+                imagesUrl.map((_, index) => (
+                    <ImageIndex 
+                        key={String(index)}
+                        active={index === imageIndex} 
+                    />      
+                ))
+            }
+        </ImageIndexes>
+        <FlatList 
+            data={imagesUrl}
+            keyExtractor={key => key}
+            renderItem={renderItem}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            onViewableItemsChanged={onIndexChanged.current}
+        />
     </Container>
   );
 }
